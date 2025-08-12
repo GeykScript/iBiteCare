e9881435a4714bdf084dfa237a777778c6506156
GeykScript <JeikuDevs@gmail.com>
2025-08-12 21:51:49 +0800

    Add design to patient website dashboard. - navigation - nav responsiveness

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,29 +1,59 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100 ">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+
+                    <a href="{{ route('dashboard') }}" class="flex items-center space-x-3 rtl:space-x-reverse hover:outline-none focus:outline-none">
+                        <img src="{{ asset('drcare_logo.png') }}" class="h-10 w-10" alt="Dr.Care Logo" />
+                        <span class="font-900 text-2xl font-bold text-[#FF000D] whitespace-nowrap">Dr.Care</span>
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
+                    <a href="#" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent 
+                       text-sm font-medium leading-5 text-gray-900 hover:text-red-600 
+                       hover:border-red-300 focus:outline-none focus:text-red-600 
+                       focus:border-red-300 transition">
+                        {{ __('Home') }}
+                    </a>
+
+                    <a href="#" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent 
+                       text-sm font-medium leading-5 text-gray-900 hover:text-red-600 
+                       hover:border-red-300 focus:outline-none focus:text-red-600 
+                       focus:border-red-300 transition">
+                        {{ __('Appointments') }}
+                    </a>
+                    <a href="#" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent 
+                       text-sm font-medium leading-5 text-gray-900 hover:text-red-600 
+                       hover:border-red-300 focus:outline-none focus:text-red-600 
+                       focus:border-red-300 transition">
+                        {{ __('Schedules') }}
+                    </a>
+                    <a href="#" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent 
+                       text-sm font-medium leading-5 text-gray-900 hover:text-red-600 
+                       hover:border-red-300 focus:outline-none focus:text-red-600 
+                       focus:border-red-300 transition">
+                        {{ __('Messages') }}
+                    </a>
                 </div>
+
             </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-900 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                            <div class="flex items-center gap-2">
+                                <i data-lucide="circle-user"></i>
+                                <h1>
+                                    {{ Auth::user()->name }}
+                                </h1>
+                            </div>
 
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -43,7 +73,7 @@
                             @csrf
 
                             <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
+                                onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
@@ -66,17 +96,39 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
+        <div class="pt-2 p-4 flex flex-col gap-2">
+            <a href="#" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent 
+                       text-sm font-medium leading-5 text-gray-900 hover:text-red-600 
+                       hover:border-red-300 focus:outline-none focus:text-red-600 
+                       focus:border-red-300 hover:bg-red-100 focus:bg-red-100 transition">
+                {{ __('Home') }}
+            </a>
+
+            <a href="#" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent 
+                       text-sm font-medium leading-5 text-gray-900 hover:text-red-600 
+                       hover:border-red-300 focus:outline-none focus:text-red-600 
+                       focus:border-red-300 hover:bg-red-100 focus:bg-red-100 transition">
+                {{ __('Appointments') }}
+            </a>
+            <a href="#" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent 
+                       text-sm font-medium leading-5 text-gray-900 hover:text-red-600 
+                       hover:border-red-300 focus:outline-none focus:text-red-600 
+                       focus:border-red-300 hover:bg-red-100 focus:bg-red-100 transition">
+                {{ __('Schedules') }}
+            </a>
+            <a href="#" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent 
+                       text-sm font-medium leading-5 text-gray-900 hover:text-red-600 
+                       hover:border-red-300 focus:outline-none focus:text-red-600 
+                       focus:border-red-300 hover:bg-red-100 focus:bg-red-100 transition">
+                {{ __('Messages') }}
+            </a>
         </div>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
+            <div class="px-4 flex gap-2">
+                <i data-lucide="circle-user"></i>
                 <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
             </div>
 
             <div class="mt-3 space-y-1">
@@ -89,7 +141,7 @@
                     @csrf
 
                     <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
+                        onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
