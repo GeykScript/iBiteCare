8d17f8672bb62d2b9f61b6b97e3588a6fd38e523
GeykScript <JeikuDevs@gmail.com>
2025-09-11 23:22:53 +0800

    Fix design in Clinic Login and changes Email Env

--- resources/views/auth/ClinicUser/two-factor.blade.php
+++ resources/views/auth/ClinicUser/two-factor.blade.php
@@ -19,63 +19,65 @@
 </head>
 
 
-<body class="bg-gradient-to-r from-red-600 to-gray-800">
+<body class="bg-gray-300">
     <section class="flex items-center justify-center md:h-screen mb-5 mt-5 md:mb-0 md:mt-0">
         <div class="flex flex-col md:flex-row justify-center items-center ">
-            <div class="md:h-[600px] md:w-[22rem] h-[16rem] w-[20rem] shadow-lg bg-[#EB1C26] md:rounded-l-[15px]  p-2 rounded-t-[15px] md:rounded-r-none md:overflow-none overflow-hidden">
+            <div class="md:h-[600px] md:w-[25rem] h-[16rem] w-[20rem] shadow-2xl bg-[#EB1C26] md:rounded-l-[10px]  p-2 rounded-t-[15px] md:rounded-r-none md:overflow-none overflow-hidden">
                 <a href="{{ route('clinic.login') }}" class="hover:outline-none focus:outline-none md:overflow-none overflow-hidden">
                     <img src="{{asset('Frame 3.png')}}" alt="" class="w-50 h-50 md:mt-0 mt-[-5rem]" />
                 </a>
             </div>
-            <div class="md:h-[37.5rem] bg-white md:w-[38rem]  w-[20rem] h-[30rem] md:rounded-r-[15px] rounded-b-[10px] md:rounded-b-[0px] shadow-lg items-center justify-center p-5 md:p-20">
-
-
-                <div class="flex flex-col justify-center ">
-
-                    <!--Form-->
-                    <div class="flex items-end justify-end mb-5">
-                        <div id="datetime" class="md:text-md text-sm text-black font-bold"></div>
+            <div class="md:h-[37.5rem] bg-white md:w-[35rem] border border-gray-200 w-[20rem] h-[32rem] md:rounded-r-[10px] rounded-b-[5px] md:rounded-b-[0px] shadow-xl items-center justify-center py-8 px-4 md:px-8 ">
+                <div class="flex flex-col justify-between h-full overflow-y-auto scrollbar-hidden">
+                    <div class="flex  justify-between mb-5">
+                        <a href="{{route('clinic.login')}}" class="flex items-center justify-center gap-2 hover:underline text-red-500  underline-offset-4">
+                            <i data-lucide="circle-chevron-left" class="text-red-500"></i>
+                            <span class="text-sm  ">Back</span>
+                        </a>
+                        <div class="flex items-center justify-between gap-3">
+                            <div id="datetime" class="md:text-md text-sm text-black font-bold"></div>
+                        </div>
                     </div>
-                    <!-- Session Status -->
-                    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-                    <form method="POST" action="{{ route('clinic.two-factor.verify') }}" class="mt-5">
-                        @csrf
-
-                        <!-- Email Address -->
-                        <input type="hidden" name="account_id" value="{{ $clinicUser->account_id }}">
-
-                        <h1 class="text-center text-xl font-bold text-black">Two-Factor Authentication</h1>
-                        <p class="text-sm mt-6">We’ve sent a 6-digit verification code to your registered email. Enter the code below to confirm your identity.</p>
-
-                        <div class="mt-4">
-                            <x-input-label for="code" :value="__('Verification Code')" />
-                            <x-input-error :messages="$errors->get('code')" class="bg-red-200 px-4 py-2 mt-5 rounded-sm" />
-
-                            <div class="flex justify-center gap-2 mt-5">
-                                @for ($i = 0; $i < 6; $i++)
-                                    <input
-                                    type="text"
-                                    maxlength="1"
-                                    name="code[]"
-                                    class="w-12 h-14 text-center border-b-2 border-gray-400 focus:border-sky-500 focus:outline-none text-2xl rounded"
-                                    oninput="moveToNext(this)"
-                                    onkeydown="moveToPrev(event, this)">
-                                    @endfor
+                    <div class="flex flex-col justify-center  gap-4">
+                        <div class="flex flex-col justify-between  md:px-12 ">
+                            <div class="w-full flex flex-col items-center  ">
+                                <x-auth-session-status class="bg-green-100 text-green-500 px-4 py-2 rounded-sm  mt-2   w-full" :status="session('status')" />
                             </div>
+                            <form method="POST" action="{{ route('clinic.two-factor.verify') }}" class="mt-5">
+                                @csrf
+                                <!-- Email Address -->
+                                <input type="hidden" name="account_id" value="{{ $clinicUser->account_id }}">
+                                <h1 class="text-center text-xl font-900 text-gray-700">Two-Factor Authentication</h1>
+                                <p class="text-md mt-6">We’ve sent a 6-digit verification code to your registered email. Enter the code below to confirm your identity.</p>
+                                <div class="mt-4">
+                                    <x-input-label for="code" :value="__('Verification Code')" class="font-900 text-md " />
+                                    <x-input-error :messages="$errors->get('code')" class="bg-red-200 px-4 py-2 mt-5 rounded-sm font-bold" />
 
-                            <input type="hidden" name="code" id="code">
+                                    <div class="flex justify-center gap-2 mt-5">
+                                        @for ($i = 0; $i < 6; $i++)
+                                            <input
+                                            type="text"
+                                            maxlength="1"
+                                            name="code[]"
+                                            class="md:w-12 w-10 h-12 md:h-14 text-center border-b-2 border-gray-400 focus:border-sky-500 focus:outline-none text-lg md:text-2xl rounded"
+                                            oninput="moveToNext(this)"
+                                            onkeydown="moveToPrev(event, this)">
+                                            @endfor
+                                    </div>
+
+                                    <input type="hidden" name="code" id="code">
+                                </div>
+
+                                <div class="mt-5">
+                                    <x-primary-button class="w-full justify-center bg-red-600 hover:bg-red-700 focus:bg-red-700 py-3">
+                                        {{ __('Verify') }}
+                                    </x-primary-button>
+                                </div>
+                            </form>
                         </div>
-
-                        <div class="mt-5">
-                            <x-primary-button class="w-full justify-center bg-red-600 hover:bg-red-700 focus:bg-red-700">
-                                {{ __('Verify') }}
-                            </x-primary-button>
-                        </div>
-                    </form>
-
-                    <div class="flex items-end justify-end md:mt-12 mt-2 text-sm text-gray-400 ">
-                        <p>iBiteCare<sup>+</sup></p>
+                    </div>
+                    <div class="flex items-end justify-end md:mt-6 mt-2 text-sm text-gray-400 ">
+                        <p class="hover:text-red-500 font-900">iBiteCare<sup>+</sup></p>
                     </div>
                 </div>
             </div>
@@ -105,8 +107,6 @@
         codeInputs.forEach(input => code += input.value);
         document.getElementById("code").value = code;
     }
-
-
 </script>
 <!-- Footer -->
 
--- resources/views/auth/ClinicUser/update-password.blade.php
+++ resources/views/auth/ClinicUser/update-password.blade.php
@@ -19,54 +19,51 @@
 </head>
 
 
-<body class="bg-gradient-to-r from-red-600 to-gray-800">
+<body class="bg-gray-300">
     <section class="flex items-center justify-center md:h-screen mb-5 mt-5 md:mb-0 md:mt-0">
         <div class="flex flex-col md:flex-row justify-center items-center ">
-            <div class="md:h-[600px] md:w-[22rem] h-[16rem] w-[20rem] shadow-lg bg-[#EB1C26] md:rounded-l-[15px]  p-2 rounded-t-[15px] md:rounded-r-none md:overflow-none overflow-hidden">
+            <div class="md:h-[600px] md:w-[25rem] h-[16rem] w-[20rem] shadow-2xl bg-[#EB1C26] md:rounded-l-[10px]  p-2 rounded-t-[15px] md:rounded-r-none md:overflow-none overflow-hidden">
                 <a href="{{ route('clinic.login') }}" class="hover:outline-none focus:outline-none md:overflow-none overflow-hidden">
                     <img src="{{asset('Frame 3.png')}}" alt="" class="w-50 h-50 md:mt-0 mt-[-5rem]" />
                 </a>
             </div>
-            <div class="md:h-[37.5rem] bg-white md:w-[38rem]  w-[20rem] h-[30rem] md:rounded-r-[15px] rounded-b-[10px] md:rounded-b-[0px] shadow-lg items-center justify-center p-5 md:p-20">
-
-
-                <div class="flex flex-col justify-center ">
-
-                    <!--Form-->
-                    <div class="flex items-end justify-end mb-5">
-                        <div id="datetime" class="md:text-md text-sm text-black font-bold"></div>
+            <div class="md:h-[37.5rem] bg-white md:w-[35rem] border border-gray-200 w-[20rem] h-[32rem] md:rounded-r-[10px] rounded-b-[5px] md:rounded-b-[0px] shadow-xl items-center justify-center p-8  ">
+                <div class="flex flex-col justify-start overflow-y-auto scrollbar-hidden h-full">
+                    <div class="flex items-end justify-between md:px-8">
+                        <div class="flex items-center justify-end md:mt-6 mt-2 text-sm text-gray-400 ">
+                            <p class="hover:text-red-500 font-900">iBiteCare<sup>+</sup></p>
+                        </div>
+                        <div id="datetime" class="md:text-md text-sm text-gray-700 font-bold"></div>
                     </div>
-                    <!-- Session Status -->
-                    <x-auth-session-status class="mb-4" :status="session('status')" />
+                    <div class="flex flex-col justify-center items-center gap-2  md:px-12">
+                            <div class="w-full flex flex-col items-center  mt-4">
+                                <x-auth-session-status class="bg-green-100 text-green-500 px-4 py-2 rounded-sm   w-full " :status="session('status')" />
+                            </div>
+                            <form method="POST" action="{{ route('clinic.update-password.update') }}" class="flex flex-col gap-3">
+                                @csrf
+                                <input type="text" name="account_id" value="{{ $clinicUser->account_id }}" hidden>
+                                <!-- Email Address -->
+                                <h1 class="items-center justify-center gap-2 md:text-xl font-900 text-gray-700 flex mt-4 "><img src="{{asset('drcare_logo.png')}}" alt="Dr.Care logo" class="w-10 h-10">Update Password</h1>
+                                <p class="text-sm ">Set a new password for your account. Use a strong password with at least 8 characters, including letters, numbers, and symbols.</p>
+                                <div>
+                                    <x-input-label for="update_clinic_password" :value="__('New Password')" />
+                                    <x-text-input id="update_clinic_password" name="password" type="password"
+                                        class="mt-1 block w-full" autocomplete="new-password" />
+                                    <x-input-error :messages="$errors->get('password')" class="bg-red-200 px-4 py-2 mt-2" />
+                                </div>
 
-                    <form method="POST" action="{{ route('clinic.update-password.update') }}" class="mt-5 flex flex-col gap-3">
-                        @csrf
-                        <input type="text" name="account_id" value="{{ $clinicUser->account_id }}" hidden>
-                        <!-- Email Address -->
-                        <h1 class="text-center text-xl font-bold text-black">Update Account Password</h1>
-                        <p class="text-sm mt-6">Set a new password for your account. Use a strong password with at least 8 characters, including letters, numbers, and symbols.</p>
-                        <div>
-                            <x-input-label for="update_clinic_password" :value="__('New Password')" />
-                            <x-text-input id="update_clinic_password" name="password" type="password"
-                                class="mt-1 block w-full" autocomplete="new-password" />
-                            <x-input-error :messages="$errors->get('password')" class="bg-red-200 px-4 py-2 mt-2" />
-                        </div>
-
-                        <div>
-                            <x-input-label for="update_clinic_password_confirmation" :value="__('Confirm Password')" />
-                            <x-text-input id="update_clinic_password_confirmation" name="password_confirmation" type="password"
-                                class="mt-1 block w-full" autocomplete="new-password" />
-                            <x-input-error :messages="$errors->get('password_confirmation')" class="bg-red-200 px-4 py-2 mt-2" />
-                        </div>
+                                <div>
+                                    <x-input-label for="update_clinic_password_confirmation" :value="__('Confirm Password')" />
+                                    <x-text-input id="update_clinic_password_confirmation" name="password_confirmation" type="password"
+                                        class="mt-1 block w-full" autocomplete="new-password" />
+                                    <x-input-error :messages="$errors->get('password_confirmation')" class="bg-red-200 px-4 py-2 mt-2" />
+                                </div>
 
 
-                        <div class="flex items-center justify-end gap-4">
-                            <button class="bg-sky-500 shadow-lg px-6 py-2 rounded text-white font-bold hover:bg-sky-400">{{ __('Update Password') }}</button>
-                        </div>
-                    </form>
-
-                    <div class="flex items-end justify-end md:mt-12 mt-2 text-sm text-gray-400 ">
-                        <p>iBiteCare<sup>+</sup></p>
+                                <div class="flex items-center justify-end gap-4">
+                                    <button class="w-full bg-sky-500 shadow-lg px-6 py-2 rounded text-white font-bold hover:bg-sky-400">{{ __('Update Password') }}</button>
+                                </div>
+                            </form>
                     </div>
                 </div>
             </div>
