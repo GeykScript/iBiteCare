c8ae63698d84b58fb52fa38064b1aab734b61d48
GeykScript <JeikuDevs@gmail.com>
2025-10-26 11:38:09 +0800

    Added Developers Page and Images

--- resources/views/developers.blade.php
+++ resources/views/developers.blade.php
@@ -39,7 +39,7 @@
         scroll-behavior: smooth;
     }
 </style>
-<!-- 
+
 <nav class="bg-white border-gray-200 shadow-md relative">
     <div class="w-full flex flex-wrap items-center justify-between md:px-12  p-4">
 
@@ -52,7 +52,7 @@
             @if (Route::has('login'))
             @auth
             <a href="{{ url('/dashboard') }}" class="hidden md:inline-block bg-gray-800 text-white text-sm px-4 py-2 rounded-md hover:bg-gray-700">
-               Return to Dashboard
+                Return to Dashboard
             </a>
             @else
             <a href="{{ route('login') }}" class="bg-red-600 text-white text-sm px-4 py-2 rounded-md hover:bg-red-500">
@@ -67,21 +67,135 @@
             @endif
         </div>
     </div>
-</nav> -->
+</nav>
+<!-- <body class="bg-gradient-to-b from-red-950 to-red-700"> -->
 
-<body class="bg-gray-900 flex items-center justify-center min-h-screen">
+<body class="bg-gradient-to-r from-red-900 via-red-700 to-red-500">
 
-    <div class="text-center px-6">
-        <h1 class="text-5xl md:text-7xl font-900 text-red-600 mb-4">🚧 Under Maintenance</h1>
-        <p class="text-gray-300 text-lg md:text-xl mb-6">
-            Our website is currently undergoing scheduled maintenance.<br>
-            We’ll be back shortly. Thank you for your patience!
-        </p>
-        <a href="{{ url('/') }}" class="bg-red-600 hover:bg-red-500 text-white px-6 py-3 rounded-md font-semibold transition-colors">
-            Go to Homepage
-        </a>
+    <div class="md:h-screen flex flex-col  justify-center max-w-6xl   max-sm:max-w-sm mx-auto py-4 px-4 md:px-0">
+        <div class="text-center">
+            <h2 class="text-white text-2xl md:text-4xl font-900">Meet Our Development team</h2>
+            <p class="text-white text-[15px] mt-4">Get to know the aspiring professionals who built this platform.</p>
+        </div>
+        <div class="grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 gap-x-6 gap-y-20 text-center mt-24">
+            <div class="bg-gray-100 relative rounded-md border border-transparent 
+                hover:shadow-[0_20px_50px_rgba(239,68,68,0.4)] hover:-translate-y-2 
+                hover:scale-105 transition-all duration-300 ease-in-out">
+                <div class="w-36 h-36 rounded-full inline-block border-4 border-gray-200  overflow-hidden -mt-14">
+                    <img src="{{ asset('images/developers/mark.jpg') }}"
+                        class="w-full h-full" />
+                </div>
+                <div class="py-4">
+                    <h4 class="text-slate-900 text-base font-semibold">Mark James Barreda</h4>
+                    <p class="text-slate-600 text-[13px] mt-4">Project Manager</p>
+
+                    <div class=" mt-4">
+                        <a href="https://www.facebook.com/Mark.Barreda.25" target="_blank"
+                            class="w-7 h-7 inline-flex items-center justify-center rounded-full border-0 outline-0 cursor-pointer bg-gray-100 hover:bg-gray-200">
+                            <img src="{{asset('socials/fb.svg')}}" alt="Facebook-logo" class="w-4 h-4 md:w-6 md:h-6">
+                        </a>
+                        <a href="https://github.com/Mark-games" target="_blank"
+                            class="w-7 h-7 inline-flex items-center justify-center rounded-full border-0 outline-0 cursor-pointer bg-gray-100 hover:bg-gray-200">
+                            <img src="{{asset('socials/github-mark.svg')}}" alt="github-logo" class="w-4 h-4 md:w-6 md:h-6">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-gray-100 relative rounded-md border border-transparent 
+                hover:shadow-[0_20px_50px_rgba(239,68,68,0.4)] hover:-translate-y-2 
+                hover:scale-105 transition-all duration-300 ease-in-out">
+                <div class="w-36 h-36 rounded-full inline-block border-4 border-gray-200  overflow-hidden -mt-14">
+                    <img src="{{ asset('images/developers/jake.jpg') }}"
+                        class="w-full h-full" />
+                </div>
+                <div class="py-4">
+                    <h4 class="text-slate-900 text-base font-semibold">Jervy Jake Morales</h4>
+                    <p class="text-slate-600 text-[13px] mt-1">Lead Web Developer</p>
+                    <p class="text-slate-600 text-[13px]">UI/UX Designer</p>
+                    <div class=" mt-4">
+                        <a href="https://www.facebook.com/jervy.jake.morales" target="_blank"
+                            class="w-7 h-7 inline-flex items-center justify-center rounded-full border-0 outline-0 cursor-pointer bg-gray-100 hover:bg-gray-200">
+                            <img src="{{asset('socials/fb.svg')}}" alt="Facebook-logo" class="w-4 h-4 md:w-6 md:h-6">
+                        </a>
+                        <a href="https://github.com/GeykScript" target="_blank"
+                            class="w-7 h-7 inline-flex items-center justify-center rounded-full border-0 outline-0 cursor-pointer bg-gray-100 hover:bg-gray-200">
+                            <img src="{{asset('socials/github-mark.svg')}}" alt="github-logo" class="w-4 h-4 md:w-6 md:h-6">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-gray-100 relative rounded-md border border-transparent 
+                hover:shadow-[0_20px_50px_rgba(239,68,68,0.4)] hover:-translate-y-2 
+                hover:scale-105 transition-all duration-300 ease-in-out">
+                <div class="w-36 h-36 rounded-full inline-block border-4 border-gray-200 bg-gray-200  overflow-hidden -mt-14">
+                    <img src="{{ asset('images/developers/neco.jpg') }}"
+                        class="w-full h-full" />
+                </div>
+                <div class="py-4">
+                    <h4 class="text-slate-900 text-base font-semibold">Neco Clemente</h4>
+                    <p class="text-slate-600 text-[13px] mt-4">Web Developer</p>
+                    <div class=" mt-4">
+                        <a href="https://www.facebook.com/kuneeeeeee" target="_blank"
+                            class="w-7 h-7 inline-flex items-center justify-center rounded-full border-0 outline-0 cursor-pointer bg-gray-100 hover:bg-gray-200">
+                            <img src="{{asset('socials/fb.svg')}}" alt="Facebook-logo" class="w-4 h-4 md:w-6 md:h-6">
+                        </a>
+                        <a href="https://github.com/kuuuu-knee" target="_blank"
+                            class="w-7 h-7 inline-flex items-center justify-center rounded-full border-0 outline-0 cursor-pointer bg-gray-100 hover:bg-gray-200">
+                            <img src="{{asset('socials/github-mark.svg')}}" alt="github-logo" class="w-4 h-4 md:w-6 md:h-6">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-gray-100 relative rounded-md border border-transparent 
+                hover:shadow-[0_20px_50px_rgba(239,68,68,0.4)] hover:-translate-y-2 
+                hover:scale-105 transition-all duration-300 ease-in-out">
+                <div class="w-36 h-36 rounded-full inline-block border-4 border-gray-200  overflow-hidden -mt-14">
+                    <img src="{{ asset('images/developers/francis.jpg') }}"
+                        class="w-full h-full" />
+                </div>
+                <div class="py-4">
+                    <h4 class="text-slate-900 text-base font-semibold">Francis Gerald Caisip</h4>
+                    <p class="text-slate-600 text-[13px] mt-4">UI/UX Designer</p>
+
+                    <div class=" mt-4">
+                        <a href="https://www.facebook.com/frahahahancis" target="_blank"
+                            class="w-7 h-7 inline-flex items-center justify-center rounded-full border-0 outline-0 cursor-pointer bg-gray-100 hover:bg-gray-200">
+                            <img src="{{asset('socials/fb.svg')}}" alt="Facebook-logo" class="w-4 h-4 md:w-6 md:h-6">
+                        </a>
+                        <a href="https://github.com/frahahahancis" target="_blank"
+                            class="w-7 h-7 inline-flex items-center justify-center rounded-full border-0 outline-0 cursor-pointer bg-gray-100 hover:bg-gray-200">
+                            <img src="{{asset('socials/github-mark.svg')}}" alt="github-logo" class="w-4 h-4 md:w-6 md:h-6">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-gray-100 relative rounded-md border border-transparent 
+                hover:shadow-[0_20px_50px_rgba(239,68,68,0.4)] hover:-translate-y-2 
+                hover:scale-105 transition-all duration-300 ease-in-out">
+                <div class="w-36 h-36 rounded-full inline-block border-4 border-gray-200  overflow-hidden -mt-14">
+                    <img src="{{ asset('images/developers/michael.png') }}"
+                        class="w-full h-full" />
+                </div>
+
+                <div class="py-4">
+                    <h4 class="text-slate-900 text-base font-semibold">Michael Vargas</h4>
+                    <p class="text-slate-600 text-[13px] mt-4">QA Tester</p>
+
+                    <div class=" mt-4">
+                        <a href="https://www.facebook.com/mchlvargas" target="_blank"
+                            class="w-7 h-7 inline-flex items-center justify-center rounded-full border-0 outline-0 cursor-pointer bg-gray-100 hover:bg-gray-200">
+                            <img src="{{asset('socials/fb.svg')}}" alt="Facebook-logo" class="w-4 h-4 md:w-6 md:h-6">
+                        </a>
+                        <a href="https://github.com" target="_blank"
+                            class="w-7 h-7 inline-flex items-center justify-center rounded-full border-0 outline-0 cursor-pointer bg-gray-100 hover:bg-gray-200">
+                            <img src="{{asset('socials/github-mark.svg')}}" alt="github-logo" class="w-4 h-4 md:w-6 md:h-6">
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
-
 </body>
 
 
